64bb6c30fd18ea03b4c34a2929d06b906a53f610
CassioJ2 <cassiojoao1@hotmail.com>
2026-03-15 15:14:22 -0300

    Clear tasks branch

--- src/main/github/errors.js
+++
@@ -1,8 +0,0 @@
-export class GitHubApiError extends Error {
-    constructor(message, { status, code } = {}) {
-        super(message)
-        this.name = 'GitHubApiError'
-        this.status = status ?? null
-        this.code = code ?? null
-    }
-}
